4ca17f7db927fe37facea890e8c9b5e02294f12a
momeemt <momiimt@gmail.com>
2023-08-10 13:07:02 +0900

    fix: disable mypy for opencv-python

--- shell.nix
+++ shell.nix
@@ -5,21 +5,22 @@ let
     url = "https://raw.githubusercontent.com/bschnurr/python-type-stubs/add-opencv/cv2/__init__.pyi";
     sha256 = "sha256-IkE/b5/bZe7oC95xTwxrlH3iZYnK3pNCarz8QYxVecY=";
   };
-  opencv4-includes-type-stub = pkgs.python311Packages.opencv4.overrideAttrs (oldAttrs: {
-    postInstall = oldAttrs.postInstall or "" + ''
-      mkdir -p $out/${pkgs.python3.sitePackages}/cv2
-      cp ${opencv-type-stub} $out/${pkgs.python3.sitePackages}/cv2/__init__.pyi
-    '';
-  });
+  mypy-stubs = pkgs.runCommand "mypy-stubs" {} ''
+    mkdir -p $out/stubs
+    cp ${opencv-type-stub} $out/stubs/cv2.pyi
+  '';
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     nixpkgs-fmt
     actionlint
     python311
-    black
-    mypy
-    opencv4-includes-type-stub
+    python311Packages.opencv4
+    python311Packages.black
+    python311Packages.mypy
     python311Packages.pytest
   ];
+  # shellHook = ''
+  #   export MYPYPATH=${mypy-stubs}/stubs:$MYPYPATH
+  # '';
 }
